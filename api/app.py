0f97070ec8617d55a882e9c328525e7f265fe22d
Gael Alejandro Fonseca <gfon@csu.fullerton.edu>
2025-06-26 12:19:35 -0700

    api

---
+++ api/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+import mysql.connector
+
+app = Flask(__name__)
+
+@app.route("/lawyers")
+def get_lawyers():
+    conn = mysql.connector.connect(
+        host="localhost", user="root", password="yourpassword", database="lawyers"
+    )
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM lawyer_data")
+    data = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return jsonify(data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
