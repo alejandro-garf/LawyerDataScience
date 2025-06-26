102ccb31b595e7cd96dcc5e311f08532219510aa
Gael Alejandro Fonseca <gfon@csu.fullerton.edu>
2025-06-26 13:45:24 -0700

    pivot

--- data/cleaning.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-def clean_data(df):
-    df['rating'] = pd.to_numeric(df['rating'], errors='coerce')
-    df['reviews'] = df['reviews'].str.extract(r'(\d+)').astype(float)
-    df = df.dropna(subset=['name', 'rating'])
-    return df
